1eedc8510d4abe05c7b86009c5a6e022a190ac8e
mohammad ahamd <ma3571318z@gmail.com>
2020-02-16 08:30:39 +0300

    another change fir the dashboard js file

--- js/dashboardTransition.js
+++ js/dashboardTransition.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
   $(".club-link").click(function() {
-    console.log("done1");
-
     $(".dashboard-view").load("club.html");
   });
 
   $(".player-link").click(function() {
-    console.log("done2");
     $(".dashboard-view").load("players.html");
   });
 
   $(".matches-link").click(function() {
-    console.log("done3");
     $(".dashboard-view").load("matches.html");
   });
 });
