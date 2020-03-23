829d97a9684a37e473b8fe91f45609d9a2ad3399
mohammad ahamd <ma3571318z@gmail.com>
2020-03-23 19:23:31 +0300

    dashboard is ready 90%

--- js/dashboardTransition.js
+++ js/dashboardTransition.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
   $(".matches-link").click(function() {
     $(".dashboard-view").load("matches.html");
   });
+  $(".laws-link").click(function() {
+    $(".dashboard-view").load("law.html");
+  });
+
+  $(".note-link").click(function() {
+    $(".dashboard-view").load("notification.html");
+  });
+
+  $(".jug-link").click(function() {
+    $(".dashboard-view").load("judgment.html");
+  });
+  $(".buy-link").click(function() {
+    $(".dashboard-view").load("");
+  });
 });
--- js/message-menu.js
+++ js/message-menu.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-  let isOpen = true;
+  var isOpen = true;
   $(".massages-page header nav .menu").click(function() {
     console.log("hiu");
 
     if (isOpen == true) {
       console.log("true");
       $(" .left-side").css("margin-left", "-300px");
-      $(".right-side").css("width", "100%");
-      $(".write").css("width", "100%");
-      $(".write").css("left", "0");
       isOpen = false;
     } else if (isOpen == false) {
       $(".left-side").css("margin-left", "0");
-      $(".right-side").css("width", "75%");
-      $(".write").css("width", "75%");
-      $(".write").css("left", "300px");
       isOpen = true;
     }
   });
